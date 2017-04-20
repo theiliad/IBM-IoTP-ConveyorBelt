946d47db5941b1ae6d2925851b729bbd3db7fac0
Eliad Moosavi <iliadm@ca.ibm.com>
2017-04-20 15:59:06 -0400

    Added speed adjustment buttons

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,4 +1,6 @@
-    var stop = false;
+var stop = false;
+var animationSpeed = 1.0;
+var durationMultiplier = 1.0;
 
 $(function() {
     console.log( "ready!" );
@@ -8,15 +10,15 @@ $(function() {
             $("g#box-" + index)
                 .delay(0)
                 .velocity({ translateY: -252 }, {
-                    duration: 0, 
+                    duration: durationMultiplier * 0, 
                     easing: "linear"
                 })
                 .velocity({ translateY: 0 }, {
-                    duration: 250, 
+                    duration: durationMultiplier * 250, 
                     easing: "easeInQuad"
                 })
                 .velocity({ translateX: 237.5 }, {
-                    duration: 1000, 
+                    duration: durationMultiplier * 1000, 
                     easing: "linear",
                     complete: function() { 
                         if (index === "1") {
@@ -27,29 +29,30 @@ $(function() {
                     }
                 })
                 .velocity({ translateX: 575 }, {
-                    duration: 1000, 
+                    duration: durationMultiplier * 1000, 
                     easing: "linear"
                 })
                 .velocity({ translateX: 630, translateY: 15, rotateZ: 30 }, {
-                    duration: 300, 
+                    duration: durationMultiplier * 300, 
                     easing: "linear"
                 })
                 .velocity({ translateX: 715, translateY: 150, rotateZ: 85 }, {
-                    duration: 300, 
+                    duration: durationMultiplier * 300, 
                     easing: "linear"
                 })
                 .velocity({ translateY: 400, rotateZ: 100 }, {
-                    duration: 300, 
+                    duration: durationMultiplier * 300, 
                     easing: "linear"
                 })
                 .velocity({ translateY: -252, translateX: 0, rotateZ: 0 }, {
-                    duration: 0
+                    duration: durationMultiplier * 0
                 });
         }
     }
 
     rotator();
     dropbox("1");
+    
     $("a.btn.start").addClass("disabled");
     
 
@@ -57,7 +60,7 @@ $(function() {
         $("line#rotator")
             .delay(0)
             .velocity({ rotateZ: 360 }, {
-                duration: 2000, 
+                duration: durationMultiplier * 2000, 
                 easing: "linear",
                 loop: true
             });
@@ -90,5 +93,37 @@ $(function() {
         if (!$("a.btn.start").hasClass("disabled")) {
             $("a.btn.start").addClass("disabled");
         }
+    });
+
+    function round(value, decimals) {
+        return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+    }
+
+    function updateSpeedOnScreen() {
+        $("span#speed-value").html(round(animationSpeed, 1) + "x");
+    }
+
+    $("a.speed-down").click(function() {
+        console.log("SPEED DOWN Clicked");
+        
+        durationMultiplier += 0.1;
+        animationSpeed -= 0.1;
+        
+        rotator();
+        updateSpeedOnScreen();
+
+        console.log(animationSpeed);
+    });
+
+    $("a.speed-up").click(function() {
+        console.log("SPEED UP Clicked");
+        
+        durationMultiplier -= 0.1;
+        animationSpeed += 0.1;
+
+        rotator();
+        updateSpeedOnScreen();
+
+        console.log(animationSpeed);
     });
 });
